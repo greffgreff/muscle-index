d27ffd1e7b24e6f1849ace462e2e3b53494097c4
greffgreff <greffchandler80@gmail.com>
2023-06-27 20:23:11 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { CSSProperties } from "react";
+import Front from "./components/body/Front";
+
+export default () => {
+  const frontBody: CSSProperties = { width: "300px", top: "50%", left: "50%", position: "absolute", translate: "-50% -50%" };
+  return (
+    <div className="app-container">
+      <Front style={frontBody} />
+    </div>
+  );
+};
---
+++ src/components/body/Back.tsx
@@ -0,0 +1,3 @@
+export default () => {
+  return <div>Back</div>;
+};
